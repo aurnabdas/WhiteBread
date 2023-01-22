c212ed28ce2f8e12bca4b692494ee951321026f6
Aurnab Das <aurnabdas@Aurnabs-MBP.lan>
2023-01-21 19:01:20 -0500

    organized classes and utilities, and added animation

--- index.js
+++ index.js
@@ -15,77 +15,20 @@ canvas.height = 576
 c.fillRect(0, 0, canvas.width, canvas.height) // (in total there are four arguments)the first and second paramets are the x and y positions, and the last two are the sizes 
 
 const gravity = 0.8
-class Sprite // moving images in game are called sprite
-{
-    constructor({ position, velocity, color = 'green', offset }) // this is the constructor of this class
-        {
-            this.position = position
-            this.velocity = velocity
-            this.width = 50
-            this.height = 150
-            this.lastKey
-            this.health = 100
-            this.attackBox = {
-                position: {
-                    x: this.position.x,
-                    y: this.position.y
-                },
-                offset, // going to make the enemy hitbox be forward if that makes sense
-                width: 100,
-                height: 50
-            }
-            this.color = color
-            this.isAttacking
-        }
 
-    draw() {
+const background = new Sprite({
+    position: { x:0, y:0 },
+    imageSrc: './img/madgurl.png' 
 
-        c.fillStyle = this.color
-        c.fillRect(this.position.x, this.position.y, this.width, this.height)
-            //attack box
-        if (this.isAttacking) {
-            c.fillStyle = 'yellow'
-            c.fillRect(this.attackBox.position.x, this.attackBox.position.y, this.attackBox.width, this.attackBox.height)
-        }
-    }
-    update() {
-        this.draw()
-        this.attackBox.position.x = this.position.x + this.attackBox.offset.x // makes attackbox independently track the character 
-        this.attackBox.position.y = this.position.y
+})
 
-        this.position.x += this.velocity.x
-        this.position.y += this.velocity.y
-
-        if (this.position.y + this.height + this.velocity.y >= canvas.height)
-        //if the player is above the bottom of the canvas the velocity should be zero meaning it should not be moving
-        //side note the reason why we are comparing canvas.height is because the y coordinate of these objects start at the top of the screen, therfore in order to ger the position of the bottom you want the height, which repersents bottom of the cavanas
-        {
-            this.velocity.y = 0
-        } else
-            this.velocity.y += gravity //gravity makes sures that the two players are all the way on the floor
-    }
-    attack()
-
-    {
-        this.isAttacking = true
-        setTimeout(() => {
-            this.isAttacking = false // pauses attack after 100 ms after activation
-        }, 100)
-
-    }
-
-}
-
-
-
-
-const player = new Sprite({
+const player = new Fighter({
     position: { x: 0, y: 0 },
     velocity: { x: 0, y: 0 },
     offset: { x: 0, y: 0 } // don't need to change player's attackbox since it's fine as it is
 })
 
-const enemy = new Sprite({
+const enemy = new Fighter({
     position: { x: 900, y: 0 },
     velocity: {
         x: 0,
@@ -118,56 +61,7 @@ const keys = {
     }
 }
 
-function rectangularCollision({ rectangle1, rectangle2 }) 
-{ // function made for easy code-reading for the collision of the attack boxes 
-    return (rectangle1.attackBox.position.x + rectangle1.attackBox.width >= enemy.position.x &&
-            rectangle1.attackBox.position.x <= rectangle2.position.x + rectangle2.width &&
-            rectangle1.attackBox.position.y + rectangle1.attackBox.height >= rectangle2.position.y &&
-            rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
-        ) //rec1 is player, rec2 is enemy
-}
 
-
-
-function determineWinner({player,enemy, timerId})
-{
-    clearTimeout(timerId)
-    document.querySelector('#displayText').style.display = 'flex' 
-
-    if( player.health === enemy.health)
-        {
-            document.querySelector('#displayText').innerHTML = 'Tie'
-        }
-        
-        else if(player.health > enemy.health)
-        {
-            document.querySelector('#displayText').innerHTML = 'PLAYER WINS'
-        }
-
-        else if(player.health < enemy.health)
-        {
-            document.querySelector('#displayText').innerHTML = 'ENEMY WINS'
-        }
-}
-
-let timer = 60 // this is just a variable for the timer 
-let timerId
-function decreaseTimer()
-{
-    timerId = setTimeout(decreaseTimer,1000) // this creates a infinite loop
-
-    if(timer > 0) // the timer has to be above 0, otherwise if you subtract 1 from the timer the value will be negative
-    { 
-        timer--
-        document.querySelector('#timer').innerHTML = timer // the querySelector grabs the html element with the id timer, then the .innerHTML goes into the value of the actually div, once we go that we are putting places the variable timer that we made into the div
-    
-    }
-
-    if(timer === 0)
-    {
-        determineWinner({player,enemy, timerId})
-    }   
-}
 decreaseTimer()
 
 
@@ -177,6 +71,7 @@ function animate()
     window.requestAnimationFrame(animate) // this creates a infinte loops, hence it creates a animation
     c.fillStyle = 'black'
     c.fillRect(0, 0, canvas.width, canvas.height) //this wont draw anything when we call it. this remove the paint like effect 
+    background.update()
     player.update()
     enemy.update()
 
